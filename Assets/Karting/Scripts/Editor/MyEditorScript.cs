1ba1b6c74cbb3a8a69d8da509aba3286d78fff21
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2021-05-22 12:23:59 +0300

    add files

---
+++ Assets/Karting/Scripts/Editor/MyEditorScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Karting.Scripts.Editor
+{
+    public class MyEditorScript: MonoBehaviour
+    {
+        public static void Start()
+        {
+         
+            string[] scenes =
+            {
+                "Assets/Karting/Scenes/IntroMenu.unity",
+                "Assets/Karting/Scenes/LoseScene.unity",
+                "Assets/Karting/Scenes/MainScene.unity",
+                "Assets/Karting/Scenes/WinScene.unity",
+            };
+            string locationPathName = @"C:\Users\Anatoly.Cherenkov\Documents\Builds\TolaKart_WebGL";
+            BuildPipeline.BuildPlayer(scenes, locationPathName, BuildTarget.WebGL, BuildOptions.None);
+        }
+    }
+}
